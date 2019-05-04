dd068fa80ff2c80ebb9696b7dee7e38e25b105d8
Thiago Corredor <thiago_corredor@live.com>
2019-05-03 21:20:52 -0300

    Testing the change

--- rabbitmq/example1/consumer.py
+++ rabbitmq/example1/consumer.py
@@ -18,3 +18,4 @@ channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
+THIAGO CORREDOR
